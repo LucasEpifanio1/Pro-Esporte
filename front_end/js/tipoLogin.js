7c82be73b9c802027ce1d4ecabfbcbcfb2fbe865
JuliaaCavalcante <juliacavalcantetomazz@gmail.com>
2025-11-14 20:19:55 -0300

    parte login

---
+++ front_end/js/tipoLogin.js
@@ -0,0 +1,16 @@
+function opcaoCidadao() {
+    localStorage.setItem("tipo", "cidadao");
+    window.location.href = 'index.html';
+}
+function opcaoCriador() {
+    localStorage.setItem("tipo", "criador");
+    window.location.href = 'tipoCriadorEventos.html';
+}
+function opcaoEmpresa() {
+    localStorage.setItem("tipo", "empresa");
+    window.location.href = 'index.html';
+}
+function opcaoFuncionario() {
+    localStorage.setItem("tipo", "funcionario");
+    window.location.href = 'index.html';
+}
