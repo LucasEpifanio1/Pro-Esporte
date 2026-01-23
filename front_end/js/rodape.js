280a368f12b861e88dd9fe4b7d2e4901f2091434
JuliaaCavalcante <juliacavalcantetomazz@gmail.com>
2026-01-22 21:19:09 -0300

    Update rodape.js
    correção do rodapé

--- front_end/js/rodape.js
+++ front_end/js/rodape.js
@@ -1,18 +1,20 @@
-function rodape() {
+const path = window.location.pathname;
+  const paginaAtual = path.substring(path.lastIndexOf('/') + 1);
+
   return `
-        <div class="item" onclick="irInicio()">
+        <div class="item ${paginaAtual === 'home.html' || paginaAtual === '' ? 'active' : ''}" onclick="irInicio()">
             <img src="img/home.png" class="icon" >
             <p>Início</p>
         </div>
-        <div class="item" onclick="irEventos()">
+        <div class="item ${paginaAtual === 'eventos.html' ? 'active' : ''}" onclick="irEventos()">
             <img src="img/calendar.png" class="icon">
             <p>Eventos</p>
         </div>
-        <div class="item" onclick="irTreinos()">
+        <div class="item ${paginaAtual === 'treinos.html' ? 'active' : ''}" onclick="irTreinos()">
             <img src="img/train.png" class="icon">
             <p>Treinos</p>
         </div>
-        <div class="item" onclick="irPerfil()">
+        <div class="item ${paginaAtual === 'perfil.html' ? 'active' : ''}" onclick="irPerfil()">
             <img src="img/user.png" class="icon">
             <p>Perfil</p>
         </div>
